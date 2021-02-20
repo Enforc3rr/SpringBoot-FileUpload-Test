e1df98601379b635b57a8eeae57be0a28f78cb5a
Prateek Kumar <prateekkumartiwari01@gmail.com>
2021-02-21 01:56:04 +0530

    Committing Files.

--- src/main/java/com/fileupload/file/FileApplication.java
+++ src/main/java/com/fileupload/file/FileApplication.java
@@ -2,12 +2,28 @@ package com.fileupload.file;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class FileApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FileApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedHeaders("*")
+                        .allowedMethods("POST");
+            }
+        };
     }
 
 }
--- src/main/java/com/fileupload/file/controller/FileUploadController.java
+++ src/main/java/com/fileupload/file/controller/FileUploadController.java
@@ -2,6 +2,7 @@ package com.fileupload.file.controller;
 
 import com.fileupload.file.service.FileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class FileUploadController {
     Multipart-file requests break large files into smaller chunks which makes it efficient for file uploads.
      */
     @PostMapping
+    @CrossOrigin(origins = "*" ,allowedHeaders = "*")
     public void uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
         fileUploadService.uploadFile(file);
     }
